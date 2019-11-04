ca4e1999e664daf596962f8e310c24a9b794f4a9
MatthewMarichiba <matthew@marichiba.com>
2019-11-03 23:09:53 -0800

    Added docs to README. Cleaned up script code.

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# nyansa-websummary
+# Programming Exercise: Web Analytics
+The problem statement is defined here: https://sites.google.com/a/nyansa.com/nyansa-programming-exercise/
+
+## Running my solution
+My solution is a Python script called `tally_sites.py`. You will need Python3 installed to run the script. The script uses no 3rd party packages, so it is not necessary to install any custom Python packages. 
+
+To run:
+
+1. `cd` to the root of this repo.
+2. `python3 ./tally_sites.py input.txt`
+
+## Notes
+Edge cases handled by my solution:
+
+* Basic command-line usage: What if no filename argument, or wrong number of arguments?
+* Graceful handling if input line formatting is bad
+* Years out of order - Sort dates correctly, even if timestamps span multiple years and are out-of-order
+
+
+## Big-O Analysis
+
+### Computational Analysis
+Let N represent the number of input lines in the input data file. For calculating computational complexity, the constituent terms are:
+* Reading the file: O(N)
+* Sort dates: Low cardinality of unique dates means this term will be negligent, which we can practically approximate as a constant/negligible O(1).
+* Iterate over the following terms for each unique date:
+  * Sort URLs by hit-count: **I used Python's built-in `sorted()` function which uses Timsort, an optimized version of merge-sort with worst-case performance of O(N log N)**
+  * Print line for each URL on that date: O(N)
+
+If we approximate the cardinality of dates as a constant, c, then the overall Big-O notation is:
+
+* O(N) + c(O(N log N) + O(N))
+* Equivalent to: O(N) + O(N log N) + O(N)
+* **The dominant term is O(N log N).**
+
+We can expect that the typical case will perform better than the worst case. The `N` in the dominant O(N log N) term represents the cardinality of unique URLs for a day (ie, *not* the number of lines in the file). The cardinality of URLs likey grows slower than the number of lines in the file. Furthermore, Python's `sorted()` Timsort algorithm takes advantage of naturally pre-sorted sequences, which improves the **log(N)** factor. We're sorting on unique hit-count values, which we know to have low cardinality. Therefore, there is a greater-than-random chance that many pre-sorted sequences will exist (Ex: `[1, 1, 1, 1, 3, 3, 4, 6]`), improving performance.
+
+### Memory Analysis
+In terms of space complexity, Python's Timsort may need to allocate up to N/2 pointers, where `N` represents the number of unique URLs for a given day. This equates to space complexity of **O(N)**. Given that we have enough memory to hold the entire list of unique URLs, we presumably have enough space to hold N/2 pointers. 
--- tally_sites.py
+++ tally_sites.py
@@ -1,32 +1,4 @@
-# x You’re given an input file.
-# x Each line consists of a timestamp (unix epoch in seconds) and a url separated by ‘|’ (pipe operator). 
-# x The entries are not in any chronological order.
-# x TODO: Account for mixed-case domains. Convert all to lower()
-# x Your task is to produce a daily summarized report on url hit count, organized daily (mm/dd/yyyy GMT) 
-# x the earliest date appearing first. TODO: Accommodate ordering by YEARS
-# x For each day, you should display the number of times each url is visited 
-# x in the order of highest hit count to lowest count. 
-# x Your program should take in one command line argument: input file name. 
-# x The output should be printed to stdout. 
-# o You can assume that the cardinality (i.e. number of distinct values) of hit count values and the number of days are much smaller than the number of unique URLs. 
-# o You may also assume that number of unique URLs can fit in memory, but not necessarily the entire file.
-
-# Edge case handling
-# x 1. What if bad line?
-# x 2. What about end of file?
-# x 3. Years out of order
-
-# Big-O thoughts:
-# Compute Complexity
-# Assume N is the number of lines in the file (or perhaps the unique # of URLS).
-# There are nested `for` loops; the outer loop iterates over dates, and the inner loop iterates over URLs
-# Worst case, every line is a different URL.
-# For the inner loop, we use python's sorted() function, which claims to have O(N log(N)) or better.
-# 
-# We expect the cardinality of dates to be low. Even though we loop over all dates, that loop will factor much lower than O(N).
-# I would expect it to act more like a constant multiplier to the loop time, and so it is probably safe to eliminate it from the O(N).
-#
-# Memory complexity: Python's sorted() claims to require N/2 pointers. Hopefully, since storing all N website names in memory is OK, then storing N/2 pointers will also be fine.
+# This script produces a daily summarized report on URL hit count for URLs in a given input file
 
 import time
 import sys
@@ -67,7 +39,7 @@ for line in inputFile:
 
 inputFile.close()
 
-# Prints a sorted list of url:hit key-value pairs. urlHits looks like: {"google.com": 4, "yahoo.com": 1 }
+# Prints a sorted list of key-value (url:hit) pairs. Ex: urlHits looks like: {"google.com": 4, "yahoo.com": 1 }
 def printUrls(urlHits):
     tuplesSortedByHits = sorted(urlHits.items(), key=lambda kv: kv[1], reverse=True)
     for (key, value) in tuplesSortedByHits:
@@ -76,7 +48,7 @@ def printUrls(urlHits):
 # Iterate through all dates and print the URL hits for the day
 sortedDates = sorted(urlTallies.keys())
 for dateTuple in sortedDates:
-    print(f"{dateTuple[1]}/{dateTuple[2]}/{dateTuple[0]} GMT")
+    print(f"{str(dateTuple[1]).zfill(2)}/{str(dateTuple[2]).zfill(2)}/{dateTuple[0]} GMT") # Ex: "08/20/2012 GMT"
     printUrls(urlTallies[dateTuple])
 
 if invalidLines:
